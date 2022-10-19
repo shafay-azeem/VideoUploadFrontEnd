cb117f5e2a6d0684a952ea9490b69177c7cbc7c6
faizan-99 <mfaizy605@gmail.com>
2022-10-19 17:50:00 +0500

    updated

--- components/UploadForm.js
+++ components/UploadForm.js
@@ -1,33 +1,3 @@
-// import {Button, StyleSheet, Text, View} from 'react-native';
-// import React from 'react';
-// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-// const includeExtra = true;
-// const options = {
-//   title: `Select Image or Video\n(mixed)`,
-//   type: 'library',
-//   options: {
-//     selectionLimit: 0,
-//     mediaType: 'mixed',
-//     includeExtra,
-//   },
-// };
-
-// const UploadForm = () => {
-//   const opengallery = async () => {
-//     const result = await launchImageLibrary(options);
-//     console.log(result);
-//   };
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Button title="upload" onPress={opengallery}></Button>
-//     </View>
-//   );
-// };
-
-// export default UploadForm;
-
-// const styles = StyleSheet.create({});
-
 import {
   StyleSheet,
   Text,
@@ -38,9 +8,9 @@ import {
 } from 'react-native';
 import React, {useState} from 'react';
 import ImagePicker from 'react-native-image-crop-picker';
-import {TextInput} from 'react-native-gesture-handler';
+
 import {useNavigation} from '@react-navigation/native';
-import axios from 'axios';
+
 var {width} = Dimensions.get('window');
 const includeExtra = true;
 export default function UploadForm() {
@@ -50,7 +20,14 @@ export default function UploadForm() {
   const [test, settest] = useState();
 
   const uploadVideo = async () => {
-    ImagePicker.openPicker({
+    // ImagePicker.openPicker({
+    //   mediaType: 'video',
+    // }).then(video => {
+    //   setVideos(video.path);
+    //   settest(video);
+    // });
+
+    ImagePicker.openCamera({
       mediaType: 'video',
     }).then(video => {
       setVideos(video.path);
@@ -59,44 +36,14 @@ export default function UploadForm() {
   };
 
   const PostVideo = async () => {
-    // console.log(videosr, 'kkkkkkkk');
-    // let formdata = new FormData();
-    // formdata.append('videos', {
-    //   uri: videos.path,
-    //   type: videos.mime,
-    // });
-    // await axios('https://videoupload-api.herokuapp.com/api/v1/media/create', {
-    //   method: 'post',
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data',
-    //   },
-    //   body: formdata,
-    // })
-    //   .then(response => {
-    //     console.log('video uploaded');
-    //     console.log(response);
-    //   })
-    //   .catch(err => {
-    //     console.log(err, '----');
-    //   });
-
-    console.log(
-      videos,
-      'path======',
-      test.mime,
-      'mime-------',
-      test.path,
-      '999999999',
-    );
+    console.log(test.path);
     let formdata = new FormData();
-    for (let key in videos) {
-      formdata.append('videos', videos[key], {
-        uri: test.path,
-        type: test.mime,
-      });
-    }
 
-    formdata.append('name', name);
+    formdata.append('videos', {
+      uri: test.path,
+      type: test.mime,
+      name: 'abc.mp4',
+    });
 
     await fetch('https://videoupload-api.herokuapp.com/api/v1/media/create', {
       method: 'post',
@@ -106,8 +53,7 @@ export default function UploadForm() {
       body: formdata,
     })
       .then(response => {
-        console.log('video uploaded');
-        console.log(response);
+        alert('Video Uploaded');
       })
       .catch(err => {
         console.log(err);
@@ -116,17 +62,6 @@ export default function UploadForm() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.relative}>
-        <TextInput
-          placeholder="Enter your name"
-          placeholderTextColor="#4F4F4F"
-          style={styles.inputBox}
-          textContentType="name"
-          value={name}
-          onChangeText={setName}
-        />
-      </View>
-
       <TouchableOpacity onPress={uploadVideo}>
         <View
           style={{
@@ -142,9 +77,7 @@ export default function UploadForm() {
             borderColor: 'black',
             borderWidth: 1,
           }}>
-          <Text style={{color: '#4F4F4F', fontSize: 15}}>
-            Upload Your Video
-          </Text>
+          <Text style={{color: '#4F4F4F', fontSize: 15}}>Open Camera</Text>
         </View>
       </TouchableOpacity>
 
@@ -159,7 +92,7 @@ export default function UploadForm() {
             marginRight: '20%',
           }}>
           <Text style={{textAlign: 'center', color: 'white', fontSize: 16}}>
-            View all videos
+            View All Videos
           </Text>
         </View>
       </TouchableOpacity>
@@ -175,7 +108,7 @@ export default function UploadForm() {
             marginRight: '20%',
           }}>
           <Text style={{textAlign: 'center', color: 'white', fontSize: 16}}>
-            Submit
+            Upload
           </Text>
         </View>
       </TouchableOpacity>
